60c79a2e6377cf377a8a367aab3c1130d168c3ef
Stuart Lustig <stueyrama@gmail.com>
2019-02-26 09:54:02 -0500

    a robot that does nothing

--- EclipseRobots/src/StuBot.java
+++ EclipseRobots/src/StuBot.java
@@ -1,7 +1,23 @@
+import robocode.HitWallEvent;
 import robocode.Robot;
+import robocode.ScannedRobotEvent;
+import java.awt.Color;
 
 public class StuBot extends Robot {
-	public StuBot() {
+	
+	public void run() {
+		setColors(Color.BLUE, Color.RED, Color.GREEN);
+		
+		while (true) {
+			
+		}
+	}
+	
+	public void onScannedRobot(ScannedRobotEvent e) {
+		
+	}
+	
+	public void onHitWall(HitWallEvent e) {
 		
 	}
 }
