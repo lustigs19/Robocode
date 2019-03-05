39129ecf82d1cb9a13106bc21852caa779560969
Stuart Lustig <stueyrama@gmail.com>
2019-03-05 08:56:03 -0500

    memmet

--- EclipseRobots/src/assets/Vec2D.java
+++ EclipseRobots/src/assets/Vec2D.java
@@ -4,11 +4,16 @@ public class Vec2D {
 	double r;
 	double theta;
 	
-	public static Vec2D makeCart(double x, double y) {
-		return new Vec2D();
+//	public static Vec2D makeCart(double x, double y) {
+//		//return new Vec2D); TODO
+//	}
+	
+	public static Vec2D makePolar(double r, double theta) {
+		return new Vec2D(r, theta);
 	}
 	
-	public static Vec2D makePolar() {
-		return new Vec2D();
+	private Vec2D(double s, double angle) {
+		r = s;
+		theta = angle;
 	}
 }
